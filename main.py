b9f9961c826d805ca0a07552fe2a57d58863f505
Jacob <jacoburl@umich.edu>
2018-10-26 15:01:50 -0400

    Works for addition

--- main.py
+++ main.py
@@ -3,9 +3,17 @@
 
 
 def calculate(arg):
-    pass
-
-
+    tokens = ['+','-','*','/']
+    stack = []
+    arg = arg.split()
+    for x in arg:
+        try:
+            stack.append(int(x))
+        except ValueError:
+            v1 = stack.pop()
+            v2 = stack.pop()
+            stack.append(v1 + v2)
+    return stack[0]
 def main():
     while True :
         calculate(input('rpn calc> '))
